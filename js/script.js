52c5cabbca307e0a36b9344eaeb20a3c387cbb14
Benjamin Ramirez <benjaramirez2003@hotmail.com>
2025-07-02 19:45:08 -0300

    primer commit con todas las paginas y estilos

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    
+    const hamburgerBtn = document.querySelector('.hamburger-btn');
+    const navMenu = document.querySelector('.nav-menu');
+
+    
+    hamburgerBtn.addEventListener('click', () => {
+
+        hamburgerBtn.classList.toggle('is-active');
+        navMenu.classList.toggle('is-active');
+    });
+
+});
